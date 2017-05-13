875cfb3a64d4b0a31cbf4ececeebb019b7a886cc
wraikes <wraikes@hotmail.com>
2017-05-13 15:30:36 -0400

    Add clean data functions

--- data_processing/data_clean.py
+++ data_processing/data_clean.py
@@ -1,63 +1,111 @@
-import os, pandas as pd, numpy as np
+import os, pandas as pd, numpy as np, matplotlib.pyplot as plt
 
 #remove when ready
-os.chdir('/home/wraikes/Programming/Personal_Projects/HorseRacing/Data/data_files')
-hr_df = pd.read_csv("HR_DF.csv")
+#os.chdir('/home/wraikes/Programming/Personal_Projects/HorseRacing/Data/data_files')
+#hr_df = pd.read_csv("HR_DF.csv")
 
 
 ################################ Distance Conversion
-distances = ['PP_{}_distance'.format(x) for x in range(1, 13)]
-distances.insert(0, 'distance')
-
-units = ['PP_{}_disttype'.format(x) for x in range(1, 13)]
-units.insert(0, 'dist_unit')
-
-dist_units = zip(distances, units)
-
-for i in dist_units:
-    hr_df[i[0]] = hr_df.apply(lambda x: x[i[0]] * 3 if x[i[1]] == 'Y' else x[i[0]], axis = 1)
-    del hr_df[i[1]]
 
 
+def dist_change(df):
+    distances = ['PP_{}_distance'.format(x) for x in range(1, 13)]
+    distances.insert(0, 'distance')
+
+    units = ['PP_{}_disttype'.format(x) for x in range(1, 13)]
+    units.insert(0, 'dist_unit')
+
+    dist_units = zip(distances, units)
+
+    for i in dist_units:
+        df[i[0]] = df.apply(lambda x: x[i[0]] * 3 if x[i[1]] == 'Y' else x[i[0]], axis = 1)
+        del df[i[1]]
+
+    return df
+    
 ################################# Drop Columns
-cols_to_drop = ['worktext', 'worknum', 'abbrevcond', 'aboutdist',
-                'agerestric', 'claimprice', 'complined2',
-                'complined3', 'complinedq', 'complineh2',
-                'complineh3', 'complineho', 'country',
-                'disttype', 'domesticpp', 'dqindicato',
-                'foreignspe', 'jockdisp', 'jockfirst',
-                'jocklast', 'jockmiddle', 'jocksuffix',
-                'lineafter', 'linebefore', 'longcommen',
-                'pulledofft', 'racedate', 'racegrade',
-                'racetype', 'sealedtrac', 'sexrestric',
-                'shortcomme', 'statebredr', 'trackcode',
-                'trackname', 'vd_claim', 'vd_reason',
-                'winddirect', 'windspeed', 'equipment',
-                'racebreed', 'apprweight', 'surface',
-                'courseid', 'trackcondi', 'J_SD_Last30_earnings', 
-                'J_jock_disp', 'T_SD_Last30_earnings',
-                'T_tran_disp', 'ae_flag', 'bet_opt', 
-                'J_stat_breed', 'breeder', 'dam', 'dist_unit', 
-                'dist_disp', 'owner_name', 'post_time', 'program', 
-                'sire', 'equip', 'race_text', 'raceord', 
-                'send_track', 'T_stat_breed', 'breed_type', 
-                'wh_foaled', 'stkorclm', 'color']
+def col_drop(df):
 
-for i in cols_to_drop:
-    nums = hr_df.columns.str.contains(i)
-    hr_df = hr_df.drop(hr_df.columns[nums], axis = 1)
+    cols_to_drop = ['worktext', 'worknum', 'abbrevcond', 'aboutdist',
+                    'agerestric', 'claimprice', 'complined2',
+                    'complined3', 'complinedq', 'complineh2',
+                    'complineh3', 'complineho', 'country',
+                    'disttype', 'domesticpp', 'dqindicato',
+                    'foreignspe', 'jockdisp', 'jockfirst',
+                    'jocklast', 'jockmiddle', 'jocksuffix',
+                    'lineafter', 'linebefore', 'longcommen',
+                    'pulledofft', 'racedate', 'racegrade',
+                    'racetype', 'sealedtrac', 'sexrestric',
+                    'shortcomme', 'statebredr', 'trackcode',
+                    'trackname', 'vd_claim', 'vd_reason',
+                    'winddirect', 'windspeed', 'equipment',
+                    'racebreed', 'apprweight', 'surface',
+                    'courseid', 'trackcondi', 'J_SD_Last30_earnings', 
+                    'J_jock_disp', 'T_SD_Last30_earnings',
+                    'T_tran_disp', 'ae_flag', 'bet_opt', 
+                    'J_stat_breed', 'breeder', 'dam', 'dist_unit', 
+                    'dist_disp', 'owner_name', 'post_time', 'program', 
+                    'sire', 'equip', 'race_text', 'raceord', 
+                    'send_track', 'T_stat_breed', 'breed_type', 
+                    'wh_foaled', 'stkorclm', 'color']
+
+    for i in cols_to_drop:
+        nums =df.columns.str.contains(i)
+        df.drop(df.columns[nums], axis = 1, inplace = True)
+    
+    return df
     
 ################################### Create new features
 
-hr_df['win'] = hr_df.where(hr_df['results'] == 1, 1, 0)
+def new_features(df):
+    df['win'] = np.where(df['results'] == 1, 1, 0)
 
-#morning odds
-morning_odds = hr_df.morn_odds.str.split('/', expand = True)
-hr_df['mo_1'] = morning_odds.iloc[:, 0].astype(int)
-hr_df['mo_2'] = morning_odds.iloc[:, 1].astype(int)
-hr_df = hr_df.drop('morn_odds', axis = 1)
-hr_df['payout'] = hr_df['mo_1'] / hr_df['mo_2']
-hr_df['odds'] = hr_df['mo_2'] / (hr_df['mo_2'] + hr_df['mo_1'])
+    #morning odds
+    morning_odds = df.morn_odds.str.split('/', expand = True)
+    df['mo_1'] = morning_odds.iloc[:, 0].astype(int)
+    df['mo_2'] = morning_odds.iloc[:, 1].astype(int)
+    df['payout'] = df['mo_1'] / df['mo_2']
+
+    df['odds'] = df['mo_2'] / (df['mo_2'] + df['mo_1'])
+    df['odds'] = df['odds'] / df.groupby(['race_date', 'track', 'race']).odds.transform('sum')
+    df.drop(['morn_odds', 'mo_1', 'mo_2'], axis = 1, inplace = True)
+
+    #Recode "Others"
+    courses = ['D', 'E', 'T']
+    df['course_id'] = np.where(df['course_id'].isin(courses), df['course_id'], 'Other')
+
+    sex = ['C', 'F', 'G', 'M']
+    df['sex'] = np.where(df['sex'].isin(sex), df['sex'], 'Other')
+
+    #Create CV column
+    df['cv'] = df.groupby(['race_date', 'track', 'race']).grouper.group_info[0]
+
+    return df
+    
+################################### Remove features with many NaNs
+
+#Explore trade-offs of cutoff values
+#cutoff = np.linspace(0, .2, 200)
+#df_shapes = []
+#
+#for i in cutoff:
+#    df_shapes.append(sum(1 - hr_df.count() / hr_df.shape[0] < i))
+#
+#df_shapes = pd.Series(df_shapes)
+#df_shapes.index = cutoff
+#
+#plt.plot(df_shapes)
+
+#Chose 0.15 with thought of filling in NaNs
+
+def remove_na_col(df):
+    results = df.results
+    speed_rating = df.speed_rating
+    new_index = 1 - df.count() / df.shape[0] < .15
+    df = df.iloc[:, new_index.values]
+    df['results'] = results
+    df['speed_rating'] = speed_rating
+    return df
 
 
 
--- data_processing/merge_data.py
+++ data_processing/merge_data.py
@@ -11,6 +11,7 @@ os.chdir('/home/wraikes/Programming/Personal_Projects/HorseRacing/Code/')
 
 from data_processing.pp_process import *
 from data_processing.results_process import *
+from data_processing.data_clean import *
 
 file_pp = '/home/wraikes/Programming/Personal_Projects/HorseRacing/Data/xml/pp'
 pp = xml_transform_pp(file_pp)
@@ -21,9 +22,13 @@ results = xml_transform_results(file_results)
 hr_df = pp.merge(results, how = 'left', on = ['horse_name', 'race_date', 'race', 'track'])
 del pp, results
 
+hr_df = hr_df.apply(pd.to_numeric, errors = 'ignore')
+
 ###Run cleaning script
-
-
+hr_df = dist_change(hr_df)
+hr_df = col_drop(hr_df)
+hr_df = new_features(hr_df)
+hr_df = remove_na_col(hr_df)
 
 hr_df.to_csv('/home/wraikes/Programming/Personal_Projects/HorseRacing/Data/data_files/HR_DF.csv')
 
